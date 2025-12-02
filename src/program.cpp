42c9d2d4ae084a84663cecc3db8095507d7610ef
Giuseppe Lanna <49965024+King-witcher@users.noreply.github.com>
2025-12-02 00:09:34 -0300

    adicionei um comentário

--- src/program.cpp
+++ src/program.cpp
@@ -6,6 +6,7 @@ int main()
   MLP mlp(3, 5, 2, 0.01, 1000);
   // Chega o mouse na frente do init_weights pra ver o que aparece.
   mlp.init_weights();
+  // Clica do lado do número da linha pra botar uma bolinha vermelha e roda no modo debug pra ver o que acontece.
 
   std::cout << "Hello, Ebsments!" << std::endl;
   return 0;
